afec449128e592b01610c6150222b00f7c656835
justin8491 <justin77748292@gmail.com>
2024-05-30 11:53:28 +0900

    ArrayList Get Set Remove Save

---
+++ src/kr/co/dong/arraylist/ArrayListTest01.java
@@ -0,0 +1,70 @@
+package kr.co.dong.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest01 {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+
+		list.add("포도"); // idx 0
+		list.add("사과"); // idx 1
+		list.add("딸기"); // idx 2
+
+		System.out.println("GET Start");
+		// 출력
+//		System.out.println(list.get(0));
+//		System.out.println(list.get(1));
+//		System.out.println(list.get(2));
+
+		for (String str : list) {
+			System.out.println(str);
+		}
+
+		// set
+		list.set(0, "오렌지");
+		list.set(1, "딸기");
+		list.set(2, "사과");
+		list.add(3, "수박");
+
+		System.out.println("SET Start");
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+		System.out.println(list.get(3));
+
+//		for (String str : list) {
+//			if (str.equals("딸기")) {
+//				list.remove(list.indexOf("딸기"));
+//			}
+//		}
+
+		String delStr = "딸기";
+		System.out.println("딸기 Delete");
+		int idx = -1;
+		for (String str : list) {
+			System.out.println(str);
+			if (delStr.equals(str)) {
+				System.out.println("있어?..");
+				idx = list.indexOf("딸기");
+				break;
+			}
+		}
+
+		if (idx == -1) {
+			System.out.println("찾는 데이터가없습니다.");
+		} else {
+			list.remove(list.indexOf("딸기"));
+			System.out.println("삭제 되었습니다.");
+			System.out.println();
+
+			System.out.println("삭제 후 출력 ");
+			for (String str : list) {
+				System.out.println(str);
+			}
+		}
+
+	}
+
+}
